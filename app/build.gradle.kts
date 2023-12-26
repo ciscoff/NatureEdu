4e6cab50bd0e050947cdd2062ef24a3b32e390ac
Sergey Yarlykov <siyarlykov@mts.ru>
2023-12-26 18:20:58 +0300

    added app name builder script

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,52 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
+val apkFileName: String by project
+val versionFileName: String by project
+val keyBuild: String by project
+val keyMinor: String by project
+val keyMajor: String by project
+
+var buildNum: Int = 0
+var minorNum: Int = 0
+var majorNum: Int = 0
+
+fun readVersions() {
+
+    val data = mutableMapOf<String, Int>()
+
+    File(versionFileName).useLines { seq ->
+        seq.forEach { line ->
+            val chunks = line.split("=")
+            if (chunks.size == 2) {
+                data[chunks[0]] = chunks[1].toInt()
+            }
+        }
+    }
+
+    var buildVersion = (data[keyBuild] ?: 0) + 1
+    var minorVersion = data[keyMinor] ?: 0
+    var majorVersion = data[keyMajor] ?: 0
+
+    if (buildVersion > 999) {
+        buildVersion = 0
+        minorVersion += 1
+        if (minorVersion > 99) {
+            minorVersion = 0
+            majorVersion += 1
+        }
+    }
+
+    buildNum = buildVersion
+    minorNum = minorVersion
+    majorNum = majorVersion
+}
+
+fun saveVersions() {
+    val fileContent = "$keyBuild=$buildNum\n$keyMinor=$minorNum\n$keyMajor=$majorNum"
+    File(versionFileName).writeText(fileContent)
+}
+
 android {
     namespace = "dev.barabu.nature"
     compileSdk = 34
@@ -15,6 +61,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        readVersions()
+        setProperty("archivesBaseName", "${apkFileName}_${majorNum}.${minorNum}.${buildNum}")
+        saveVersions()
     }
 
     buildTypes {
@@ -41,8 +91,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-//    implementation project(":example:custom")
     implementation(project(":app:base"))
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
